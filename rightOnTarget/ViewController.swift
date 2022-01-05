7e0e5b1b18589cbafc2e7ecdf7b04aa90908d6e3
SergeyPavlovDev <mosevaly@gmail.com>
2022-01-05 17:16:34 +0300

    3. making outlets

--- rightOnTarget/ViewController.swift
+++ rightOnTarget/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
+    
 
 }
 
